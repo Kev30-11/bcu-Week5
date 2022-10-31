7effa269678b4381f155da75e5251df7110e56e5
Kev30-11 <117093425+Kev30-11@users.noreply.github.com>
2022-10-31 14:01:53 +0000

    Initial commit

---
+++ Catch up-week6 .R
@@ -0,0 +1,14 @@
+library(tidyverse)
+library(gapminder)
+
+#filter for values in europe and the year 2007
+gapminder %>%
+  pivot_longer(lifeExp:gdpPercap)%>%
+  filter(str_detect(continent, "Europe"),
+         year > 1987) %>%
+  ggplot() +
+  aes(x = year,
+      y = value, 
+      colour = name ) +
+  geom_line() +
+facet_wrap(~ country)
